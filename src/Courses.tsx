932b3a18f324830ed99839656e5de27d3680577c
Cristin O'Connor <her@cristin.io>
2023-06-18 05:52:47 -0600

    Finished courses in a nice enough way

--- src/Courses.tsx
+++ src/Courses.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader } from "@mui/material"
+import { Avatar, Card, CardContent, CardHeader } from "@mui/material"
 import React from "react";
 
 
@@ -9,11 +9,16 @@ const Courses: React.FC<{ data: any }> = ({ data }) => {
 
   return data.map((course: any, index: number) => (
     <Card key={`course-${index}`} sx={{width: '100%'}}>
+
       <CardHeader
-        title={course.title}
+        title={<div><Avatar src={course?.imageUrl} sx={{maxHeight: '80px', maxWidth: '80px', mx: 'auto', mb: 1}} /> {course.title}</div>}
         subheader={`Completed on ${course.completedDate} | ${course.platform}`}
+        sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}
+        subheaderTypographyProps={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}
+        titleTypographyProps={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}
       />
-      <CardContent>
+
+      <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         {course.description}
       </CardContent>
     </Card>
